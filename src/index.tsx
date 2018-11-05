688edd68b96bcef8f62e29f29e53d568e177ce9f
pablo <pablo.gobe@gmail.com>
2018-11-05 14:04:00 +0000

    Default to named imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from 'src/App/App';
+import { App } from 'src/App/App';
 import 'src/index.css';
 
 import registerServiceWorker from './registerServiceWorker';
