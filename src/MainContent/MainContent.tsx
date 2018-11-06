913b7b34140b52e1dfd0aaed1aad6ceaa9be3c44
pablo <pablo.gobe@gmail.com>
2018-11-06 18:10:14 +0000

    Enabled request to heroku

--- src/MainContent/MainContent.tsx
+++ src/MainContent/MainContent.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import "src/commonCSS.css";
-// import { postJSON } from 'src/utils/web';
+import { postJSON } from 'src/utils/web';
 import CircularProgress from '@material-ui/core/CircularProgress';
-// import { ProcessResponse, InitialStreamPostArguments } from "src/utils/interfaces";
+import { ProcessResponse, InitialStreamPostArguments } from "src/utils/interfaces";
 import { Search } from "src/Search/Search";
 import { LanguageSuggestion } from "src/utils/interfaces";
 
-// const SERVER_URL = "https://vast-plains-75205.herokuapp.com";
+const SERVER_URL = "https://vast-plains-75205.herokuapp.com";
 
 interface MainContentState {
     loading: boolean;
@@ -25,9 +25,9 @@ export class MainContent extends React.Component<{}, MainContentState> {
       console.log(search);
       console.log(lang);
       this.setState({ loading: true });
-      // const postPayload: InitialStreamPostArguments = { url: search, lang: lang };
-      // const res: ProcessResponse = await postJSON<ProcessResponse, InitialStreamPostArguments>(SERVER_URL, "stream", postPayload);
-      // console.log(res);
+      const postPayload: InitialStreamPostArguments = { url: search, lang: lang.value };
+      const res: ProcessResponse = await postJSON<ProcessResponse, InitialStreamPostArguments>(SERVER_URL, "stream", postPayload);
+      console.log(res);
       this.setState({ loading: false });
     }
 
