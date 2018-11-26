e72fc346210b818e6a4a01faab0adf3a2b09a17b
namanwahi <namanwahi@yahoo.co.uk>
2018-11-26 01:01:02 +0000

    added stuff to information page, need to style now@

--- src/Information/Information.tsx
+++ src/Information/Information.tsx
@@ -1,10 +1,49 @@
 import * as React from 'react'
 import Logo from 'src/media/icon_square.png';
-import { createStyles, WithStyles, withStyles, Typography } from '@material-ui/core';
+import { createStyles, WithStyles, withStyles, Typography, Slide, withTheme, WithTheme } from '@material-ui/core';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
-export interface InformationProps extends WithStyles<typeof styles> {
-  imageWidthEm?: number
+export interface InformationProps extends WithStyles<typeof styles>{
+  imageWidth?: number
 }
+
+interface InformationState {
+  slideIndex: number;
+}
+
+interface InformationSegment {
+  title: string;
+  body: string;
+}
+
+const informationSegments : InformationSegment[] = [
+  {
+    title: "What is Polyglot?", 
+    body: "Polyglot is a hack... I mean subtitling provider for livestreams. \
+          For the affordable price of $100 per hour of streaming. You can get half-arsed translations \
+          courtesy of Google and Microsoft. i.e companies who know what they are doing."
+  },
+  { 
+    title: "What languages are supported?",
+    body: "The Polyglot has only been tested with Spanish and French however... in theory \
+           it should work for many other languages. We can not be bothered to find people of every language \
+           this supports. If you notice an error in the translation.... blame Google."
+  },
+  {
+    title: "What platforms are supported?",
+    body: "Polyglot is mainly intended for Youtube; However, we are aiming to make it work for many \
+          sites such as Twitch, BBC iPlayer and Chaturbate. In theory, it would for for any website \
+          with Streamlink compatabilities."
+  }
+];
+
 
 const styles = createStyles({
   root: {
@@ -16,22 +55,79 @@ const styles = createStyles({
   }
 });
 
-export const InformationComponent = (props: InformationProps) => {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <img src={Logo} width={props.imageWidthEm ? props.imageWidthEm.toString() + "em" : "50em"}/>
-      <Typography variant="body1" gutterBottom>
-        Sed ut perspiciatis unde omnis iste natus error sit 
-        voluptatem accusantium doloremque laudantium, totam rem
-        aperiam, eaque ipsa quae ab illo inventore veritatis et 
-        quasi architecto beatae vitae dicta sunt explicabo. Nemo 
-        enim ipsam voluptatem quia voluptas sit aspernatur 
-        aut odit aut fugit, sed quia consequuntur magni dolores
-        eos qui ratione voluptatem sequi nesciunt.
-      </Typography>        
-    </div>
-    );
+class InformationComponent extends React.Component<InformationProps & WithTheme, InformationState> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      slideIndex: 0
+    };
+  }
+
+  private handleNext = () => {
+    this.setState(prevState => ({
+      slideIndex: prevState.slideIndex + 1,
+    }));
+  };
+
+  private handleBack = () => {
+    this.setState(prevState => ({
+      slideIndex: prevState.slideIndex - 1,
+    }));
+  };
+
+  private handleStepChange = slideIndex => {
+    this.setState({ slideIndex });
+  };
+
+  render() {
+    const { classes, theme } = this.props;
+    const maxIndex = informationSegments.length;
+    const { slideIndex } =  this.state;
+
+    return (
+      <div className={classes.root}>
+        <img src={Logo} width={this.props.imageWidth ? this.props.imageWidth.toString() + "px" : "250px"}/>
+        <div>
+        <Paper square elevation={0}>
+          <Typography>{informationSegments[slideIndex].title}</Typography>
+        </Paper>
+        <AutoPlaySwipeableViews
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          index={slideIndex}
+          onChangeIndex={this.handleStepChange}
+          enableMouseEvents
+        >
+          {informationSegments.map((step, index) => (
+            <div key={step.title}>
+              {Math.abs(slideIndex - index) <= 2 ? (
+                <p>{step.body}</p>
+              ) : null}
+            </div>
+          ))}
+        </AutoPlaySwipeableViews>
+        <MobileStepper
+          steps={maxIndex}
+          position="static"
+          activeStep={slideIndex}
+          nextButton={
+            <Button size="small" onClick={this.handleNext} disabled={slideIndex === maxIndex - 1}>
+              Next
+              {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+            </Button>
+          }
+          backButton={
+            <Button size="small" onClick={this.handleBack} disabled={slideIndex === 0}>
+              {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+              Back
+            </Button>
+          }
+        />
+      </div>    
+      </div>
+      );
+  }
+
 }
 
-export const Information = withStyles(styles)(InformationComponent);
+export const Information = withStyles(styles, {withTheme : true})(InformationComponent);
