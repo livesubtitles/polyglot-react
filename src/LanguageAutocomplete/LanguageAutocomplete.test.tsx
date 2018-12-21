ff733d1c57e805cf53730087dc6c6363486a935d
pablo <pablo.gobe@gmail.com>
2018-12-21 21:14:47 +0100

    Added more tests for autocomplete

--- src/LanguageAutocomplete/LanguageAutocomplete.test.tsx
+++ src/LanguageAutocomplete/LanguageAutocomplete.test.tsx
@@ -3,6 +3,15 @@ import * as ReactDOM from 'react-dom';
 import * as TestRenderer from 'react-test-renderer';
 import { LanguageAutocomplete } from 'src/LanguageAutocomplete/LanguageAutocomplete';
 import InputLabel from "@material-ui/core/InputLabel";
+import * as Autosuggest from "react-autosuggest";
+
+function getLanguageAutocomplete(isErrorSuggestion: boolean) {
+  return TestRenderer.create(<LanguageAutocomplete
+    onSuggestionSelected={mockOnSuggestionSelected}
+    onChangeValue={mockOnChangeValue}
+    isErrorSuggestion={isErrorSuggestion}
+  />).root;
+}
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -15,16 +24,16 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
+let mockOnSuggestionSelected;
+let mockOnChangeValue;
+
+beforeEach(() => {
+  mockOnSuggestionSelected = jest.fn();
+  mockOnChangeValue = jest.fn();
+}
+
 it("calls onChangeValue when there is a change in the input", (done) => {
-  const mockOnSuggestionSelected = jest.fn();
-  const mockOnChangeValue = jest.fn();
-  const instance = TestRenderer.create(
-                      <LanguageAutocomplete
-                        onSuggestionSelected={mockOnSuggestionSelected}
-                        onChangeValue={mockOnChangeValue}
-                        isErrorSuggestion={false}
-                      />
-                   ).root;
+  const instance = getLanguageAutocomplete(false);
   const input = instance.find(el => el.type === "input");
   const testValue = "SomeValue";
   input.props.onChange({ target: { value: testValue } });
@@ -34,17 +43,18 @@ it("calls onChangeValue when there is a change in the input", (done) => {
 });
 
 it("shows invalid message if it is an error suggestion", (done) => {
-  const mockOnSuggestionSelected = jest.fn();
-  const mockOnChangeValue = jest.fn();
-  const instance = TestRenderer.create(
-                      <LanguageAutocomplete
-                        onSuggestionSelected={mockOnSuggestionSelected}
-                        onChangeValue={mockOnChangeValue}
-                        isErrorSuggestion={true}
-                      />
-                   ).root;
-
+  const instance = getLanguageAutocomplete(true);
   const input = instance.findByType(InputLabel);
   expect(input.props.children).toEqual("Invalid language");
+  done();
+});
+
+it("calls onSuggestionSelected when a suggestion is selected", (done) => {
+  const instance = getLanguageAutocomplete(false);
+  const autosuggest = instance.findByType(Autosuggest);
+  const suggestion = { label: "English", value: "en-US"};
+  autosuggest.props.onSuggestionSelected(null, { suggestion: suggestion });
+  expect(mockOnSuggestionSelected.mock.calls.length).toEqual(1);
+  expect(mockOnSuggestionSelected.mock.calls[0]).toEqual([null, { suggestion: suggestion }])
   done();
 });
