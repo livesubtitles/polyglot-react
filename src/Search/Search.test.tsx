47ccc33c31483cd9b871b6dda02146a23dc33842
pablo <pablo.gobe@gmail.com>
2018-12-21 18:50:08 +0100

    More tests for Search

--- src/Search/Search.test.tsx
+++ src/Search/Search.test.tsx
@@ -6,6 +6,7 @@ import { Search } from 'src/Search/Search';
 import { UrlInputView } from "src/UrlInputView/UrlInputView";
 import { LanguageAutocomplete } from "src/LanguageAutocomplete/LanguageAutocomplete";
 import IconButton from '@material-ui/core/IconButton';
+import { LanguageSuggestion } from "src/utils/interfaces";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -13,25 +14,48 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it("calls search when input is valid", (done) => {
+
+const URL = "www.youtube.com";
+const LANG_SUGGESTION = { label: "Spanish", value: "es-ES" }
+
+function simulateSearch(url: string, valueLabel: string, suggestion: LanguageSuggestion, numCalls: number, expectationFunc) {
   const mockfn = jest.fn();
   const instance = TestRenderer.create(
                       <Search onSearch={mockfn}/>
                    ).root;
 
-  const URL = "www.youtube.com";
-  const LANG_SUGGESTION = { label: "Spanish", value: "es-ES" }
-
   const urlview = instance.findByType(UrlInputView);
-  urlview.props.onChange({ target: { value: "www.youtube.com"}})
+  urlview.props.onChange({ target: { value: url}})
 
   const langAutocomplete = instance.findByType(LanguageAutocomplete);
-  langAutocomplete.props.onChangeValue("Spanish");
-  langAutocomplete.props.onSuggestionSelected(null, { suggestion: LANG_SUGGESTION });
-
+  langAutocomplete.props.onChangeValue(valueLabel);
+  langAutocomplete.props.onSuggestionSelected(null, { suggestion: suggestion });
   const searchButton = instance.findByType(IconButton);
   searchButton.props.onClick();
-  expect(mockfn.mock.calls.length).toEqual(1);
-  expect(mockfn.mock.calls[0]).toEqual([URL, LANG_SUGGESTION]);
+  expect(mockfn.mock.calls.length).toEqual(numCalls);
+  expectationFunc(mockfn);
+}
+
+it("calls search when input is valid", (done) => {
+
+  simulateSearch(URL, LANG_SUGGESTION.label, LANG_SUGGESTION, 1, (mockfn) => {
+    expect(mockfn.mock.calls[0]).toEqual([URL, LANG_SUGGESTION]);
+  });
+  done();
+});
+
+
+it("does not call search if url input is empty", (done) => {
+  simulateSearch("", LANG_SUGGESTION.label, LANG_SUGGESTION, 0, (mockfn) => {});
+  done();
+});
+
+it("does not call search if language typed is not in suggestions", (done) => {
+  simulateSearch(URL, "SomeRandomLanguage", LANG_SUGGESTION, 0, (mockfn) => {});
+  done();
+});
+
+it("does not call search if language typed is different from choice (so it has not been selected properly)", (done) => {
+  simulateSearch(URL, "French", LANG_SUGGESTION, 0, (mockfn) => {});
   done();
 });
