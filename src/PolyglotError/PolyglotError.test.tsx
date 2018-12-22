9e20ba27222dcea48c0acb34ea653df036682fd1
namanwahi <namanwahi@yahoo.co.uk>
2018-12-22 13:33:28 +0000

    enzyme test for polyglot error

--- src/PolyglotError/PolyglotError.test.tsx
+++ src/PolyglotError/PolyglotError.test.tsx
@@ -2,9 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { PolyglotErrorType } from "src/utils/interfaces";
 import { PolyglotError } from 'src/PolyglotError/PolyglotError';
-import * as TestRenderer from 'react-test-renderer';
-import { Select } from '@material-ui/core';
-
+import * as enzyme from 'enzyme';
+import Button from '@material-ui/core/Button';
 
 it('renders for each type of error without crashing', () => {
   [
@@ -16,4 +15,12 @@ it('renders for each type of error without crashing', () => {
     ReactDOM.render(<PolyglotError error={error} restoredError={() => {}}/>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
+});
+
+it('close button', () => {
+  const mockfn = jest.fn();
+  const instance = enzyme.shallow(<PolyglotError error={PolyglotErrorType.SocketConnection} restoredError={mockfn}/>);
+  const button  = instance.find(Button);
+  button.simulate('click');
+  expect(mockfn.mock.calls.length).toBe(1);
 });
---
+++ src/setupTests.ts
@@ -0,0 +1,7 @@
+/*
+  Setup file for enzyme
+*/
+import * as enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+
+enzyme.configure({ adapter: new Adapter() });
