cc64a7080960ba9f35f993af7f12ecb79cfbc45a
namanwahi <namanwahi@yahoo.co.uk>
2018-12-23 16:50:16 +0000

    font size slider tests

--- src/FontSizeSlider/FontSizeSlider.test.tsx
+++ src/FontSizeSlider/FontSizeSlider.test.tsx
@@ -1,9 +1,33 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { FontSizeSlider } from 'src/FontSizeSlider/FontSizeSlider';
+import * as enzyme from 'enzyme';
+import Slider from '@material-ui/lab/Slider';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<FontSizeSlider onFontSizeChange={(n) => {}}/>, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe("behaviour tests", () => {
+  let wrapper;
+  const mockfn = jest.fn();
+
+  beforeEach(() => {
+    wrapper = enzyme.shallow(<FontSizeSlider onFontSizeChange={mockfn}/>).dive();
+  });
+
+  afterEach(() => {
+    mockfn.mockClear();
+  });
+
+  it("can change size", () => {
+    const slider = wrapper.find(Slider);
+    const dummyVal = 45;
+    slider.simulate("change", {}, dummyVal);
+    expect(mockfn).toBeCalledTimes(1);
+    expect(mockfn.mock.calls[0][0]).toEqual(dummyVal);
+  });
 });
