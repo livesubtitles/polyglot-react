37acdd2a16d80ac81216eaac1fd4e44a32470ca7
namanwahi <namanwahi@yahoo.co.uk>
2018-12-23 15:15:55 +0000

    tested number of menu items on a quality dropdown

--- src/QualityDropdown/QualityDropdown.test.tsx
+++ src/QualityDropdown/QualityDropdown.test.tsx
@@ -1,9 +1,31 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { QualityDropdown } from 'src/QualityDropdown/QualityDropdown';
+import * as enzyme from 'enzyme';
+import MenuItem from '@material-ui/core/MenuItem';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<QualityDropdown qualities={["144", "360"]} onQualitySelection={(q) => {}}/>, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe("behaviour tests",() => {
+  let wrapper;
+  let mockfn;
+  const dummyQualities = ["144", "360", "480"];
+  
+  beforeEach(() => {
+    mockfn = jest.fn();
+    wrapper = 
+      enzyme.shallow(<QualityDropdown onQualitySelection={mockfn} qualities={dummyQualities} />).dive();
+  });
+
+  it("correct number of menu items", () => {
+    const menuItems = wrapper.find(MenuItem);
+    expect(menuItems).toHaveLength(dummyQualities.length);
+  }); 
+
+
 });
--- src/SubtitleLanguageDropdown/SubtitleLanguageDropdown.test.tsx
+++ src/SubtitleLanguageDropdown/SubtitleLanguageDropdown.test.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { SubtitleLanguageDropdown } from 'src/SubtitleLanguageDropdown/SubtitleLanguageDropdown';
 import * as enzyme from 'enzyme';
-import { createShallow } from '@material-ui/core/test-utils';
 import Select from '@material-ui/core/Select';
 
 it('renders without crashing', () => {
