97d8497826b1cdd7c16afcdbd168f215d167500b
namanwahi <namanwahi@yahoo.co.uk>
2018-12-23 01:07:06 +0000

    subtitle language dropdown tests

--- src/SubtitleLanguageDropdown/SubtitleLanguageDropdown.test.tsx
+++ src/SubtitleLanguageDropdown/SubtitleLanguageDropdown.test.tsx
@@ -1,9 +1,32 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { SubtitleLanguageDropdown } from 'src/SubtitleLanguageDropdown/SubtitleLanguageDropdown';
+import * as enzyme from 'enzyme';
+import { createShallow } from '@material-ui/core/test-utils';
+import Select from '@material-ui/core/Select';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<SubtitleLanguageDropdown />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+
+describe("behaviour tests", () => {
+  let mockfn;
+  let wrapper;
+
+  beforeEach(() => {
+    mockfn = jest.fn();
+    wrapper = enzyme.shallow(<SubtitleLanguageDropdown onSubtitleLanguageSelection={mockfn} />).dive();
+  });
+
+  it("test change", () => {
+    const dummyLang = "fr-FR";
+    const mockEvent = {target: {value: dummyLang}};
+    const select = wrapper.find(Select); 
+    select.simulate("change", mockEvent);
+    expect(mockfn).toHaveBeenCalledTimes(1);
+    expect(mockfn.mock.calls[0][0]).toEqual("fr");
+  });
 });
