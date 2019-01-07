6710a125a734f3aaebac5275b02d896ea6852a63
g-ongetta <georgeongetta@gmail.com>
2019-01-07 16:12:03 +0000

    Increased percentage progress from a buffer to account for new backend wait time

--- src/MainContent/MainContent.tsx
+++ src/MainContent/MainContent.tsx
@@ -226,7 +226,7 @@ class MainContentComponent extends React.Component<MainContentProps, MainContent
       hls.onBufferAppended(() => {
         console.log("Buffer appended");
         console.log(this.state.progress);
-        const INCREASE = 7;
+        const INCREASE = 20;
         // Received buffer, so increase progress
         if (this.state.progress + INCREASE >= 100) {
           self.hideLoading("loadingdiv", "videodiv");
